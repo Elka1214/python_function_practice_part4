c7e15d8bc089c0f88d4e6d2d1969a797dbc0ab7b
Elka1214 <116694312+Elka1214@users.noreply.github.com>
2023-07-05 21:29:40 -0400

    updated

--- functions.py
+++ functions.py
@@ -1,7 +1,22 @@
-def max_num(a,b,c):
- return max([a,b,c])
+def max_num(a, b, c):
+    return max([a, b, c])
 
-print(max_num(1,2,3))
-print(max_num(4,5,6))
-print(max_num(7,8,9))
 
+print(max_num(1, 2, 3))
+print(max_num(4, 5, 6))
+print(max_num(7, 8, 9))
+
+
+def mult_list(lst):
+    if len(lst) == 0:
+        return 0
+    prod = lst[0]
+    if len(lst) > 1:
+        for i in lst[1:]:
+            prod = prod * i
+    return prod
+
+
+print(mult_list([1, 2, 3]))
+print(mult_list([]))
+print(mult_list([15]))
